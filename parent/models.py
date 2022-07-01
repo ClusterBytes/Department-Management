e70e6d7befa76a38694f7d07f85642ef78725acf
AbhishekBallullayaK <abhishekbk1806@gmail.com>
2022-07-01 23:35:58 +0530

    Corrected error in models.py file of parent

--- parent/models.py
+++ parent/models.py
@@ -4,10 +4,8 @@ from student.models import profile_student
 # Create your models here.
 class parent_profile(models.Model):
     parent_id = models.BigIntegerField(primary_key=True)
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
     phone_no = models.BigIntegerField()
     email = models.EmailField()
 
-class parent_student(models.Models):
-    register_no = models.ForeignKey(profile_student)
+class parent_student(models.Model):
+    register_no = models.ForeignKey(profile_student,on_delete=models.CASCADE)
