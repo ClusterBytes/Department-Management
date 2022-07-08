3f6b5343c5baba4bd93f8fce774611eac33eafb7
AbhishekBallullayaK <abhishekbk1806@gmail.com>
2022-07-08 15:34:07 +0530

    Fixed mistake in mdels.py in parent

--- parent/models.py
+++ parent/models.py
@@ -6,6 +6,5 @@ class parent_profile(models.Model):
     parent_id = models.BigIntegerField(primary_key=True)
     phone_no = models.BigIntegerField()
     email = models.EmailField()
+    register_no = models.ForeignKey(profile_student, on_delete=models.CASCADE)
 
-class parent_student(models.Model):
-    register_no = models.ForeignKey(profile_student,on_delete=models.CASCADE)
