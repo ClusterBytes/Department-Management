6c81b359de750f4ef1143f13798c437ed3fa4d6c
AbhishekBallullayaK <abhishekbk1806@gmail.com>
2022-07-09 15:52:35 +0530

    Updated models.py in parent folder

--- parent/models.py
+++ parent/models.py
@@ -4,6 +4,8 @@ from student.models import profile_student
 # Create your models here.
 class parent_profile(models.Model):
     parent_id = models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     phone_no = models.BigIntegerField()
     email = models.EmailField()
     register_no = models.ForeignKey(profile_student, on_delete=models.CASCADE)
